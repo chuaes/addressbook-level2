5da60d2fafda59488233e98343d68719a17614f3
chuaes <c_es@rocketmail.com>
2019-02-12 16:26:13 +0800

    add the execute method in UpdateCommand to facilitate update

--- src/seedu/addressbook/commands/UpdateCommand.java
+++ src/seedu/addressbook/commands/UpdateCommand.java
@@ -1,6 +1,7 @@
 package seedu.addressbook.commands;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import seedu.addressbook.data.exception.IllegalValueException;
@@ -14,13 +15,13 @@ import seedu.addressbook.data.person.UniquePersonList;
 import seedu.addressbook.data.tag.Tag;
 
 /**
- * Update a person to the address book.
+ * Updates existing person to the address book.
  */
 public class UpdateCommand extends Command {
 
     public static final String COMMAND_WORD = "update";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates a person to the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates existing person to the address book. "
             + "Contact details can be marked private by prepending 'p' to the prefix.\n"
             + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n"
             + "Example: " + COMMAND_WORD
@@ -30,7 +31,49 @@ public class UpdateCommand extends Command {
     public static final String MESSAGE_NO_PERSON = "This person does not exist in the address book";
 
     private final Person toUpdate;
+    private final String NAME;
 
- 
+    /**
+     * Convenience constructor using raw values.
+     *
+     * @throws IllegalValueException if any of the raw values are invalid
+     */
+    public UpdateCommand(String name,
+                      String phone, boolean isPhonePrivate,
+                      String email, boolean isEmailPrivate,
+                      String address, boolean isAddressPrivate,
+                      Set<String> tags) throws IllegalValueException {
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tags) {
+            tagSet.add(new Tag(tagName));
+        }
+        this.NAME = name;
+        this.toUpdate = new Person(
+                new Name(name),
+                new Phone(phone, isPhonePrivate),
+                new Email(email, isEmailPrivate),
+                new Address(address, isAddressPrivate),
+                tagSet
+        );
+    }
+
+    public UpdateCommand(Person toUpdate) {
+        this.toUpdate = toUpdate;
+        this.NAME = toUpdate.getName().toString();
+    }
+
+    public ReadOnlyPerson getPerson() {
+        return toUpdate;
+    }
+
+    @Override
+    public CommandResult execute() {
+        try {
+            addressBook.updatePerson(toUpdate);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, toUpdate));
+        } catch (UniquePersonList.PersonNotFoundException npe) {
+            return new CommandResult(MESSAGE_NO_PERSON);
+        }
+    }
 
 }
